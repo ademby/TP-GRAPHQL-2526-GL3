a56a3fe4f8ac262a91f30974216c905c8e9968ce
ademby <adem.benyarou10@gmail.com>
2026-04-23 20:21:52 +0100

    test: add new model

--- src/script.ts
+++ src/script.ts
@@ -6,8 +6,23 @@ const prisma = new PrismaClient();
 
 // 3
 async function main() {
-  const allLinks = await prisma.link.findMany();
-  console.log(allLinks);
+  console.log("Link: ", await prisma.link.findMany());
+
+  console.log(
+    "Link2: ",
+    (await prisma.link2.findMany()).map((x) => x.id),
+  );
+  const newLink = await prisma.link2.create({
+    data: {
+      url: "www.google.com",
+      description: "Most known URL {probably}",
+      author: "KB",
+    },
+  });
+  console.log(
+    "Link2: ",
+    (await prisma.link2.findMany()).map((x) => x.id),
+  );
 }
 
 // 4
